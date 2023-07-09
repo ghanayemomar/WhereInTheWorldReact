9ae2a14918017cdd13d04f31ad12a649b90eb960
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-07-09 04:50:16 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1869,11 +1869,11 @@ const countries =[
 
 const router = createBrowserRouter([
   {
-    path: "WhereInTheWorldReact",
+    path: "/WhereInTheWorldReact",
     errorElement: <ErrorPage />,
     element: <RootLayout />,
     children: [
-      { path: "", element: <HomePage countries={countries} /> },
+      {index: true, element: <HomePage countries={countries} /> },
       { path: "Detail-Page/:CountryName", element: <DetailPage countries={countries} /> },
     ],
   },

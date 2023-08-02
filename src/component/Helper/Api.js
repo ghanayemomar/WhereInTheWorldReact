1546fe7ba871260ed906cba3652e47b3d883b09d
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-08-02 19:06:58 +0300

    updating the Api component to make the api url general

--- src/component/Helper/Api.js
+++ src/component/Helper/Api.js
@@ -1,16 +1,20 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
- const fetchCountries = async (searchResult) => {
+const generateURL = (searchResult) => {
   let url =
     "https://restcountries.com/v3.1/all?fields=name,capital,population,region,flags";
   if (searchResult) {
     url = `https://restcountries.com/v3.1/name/${searchResult}?fields=name,capital,currencies,population,region,subregion,tld,borders,flags,languages`;
   }
+  return url;
+};
+
+const fetchCountries = async (searchResult) => {
+  const url = generateURL(searchResult);
   const response = await axios.get(url);
   return response.data;
 };
-//separe them. 
 
 export const useCountries = (searchResult) => {
   return useQuery(
